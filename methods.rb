6d0d6f7b6cec311e72ca23375f0323961270c2d2
Elliott C. Woodward <elliottcwoodward@gmail.com>
2019-03-13 22:26:17 -0400

    added methods.rb

---
+++ methods.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby -wKU
+
+=begin
+1. Write a method called rock that will print “I wanna rock!” to the screen when called.
+
+
+
+2. Write a method called my_favorite_band that takes in an input called band and uses that input to state what your favorite band is.
+
+
+
+Example output:  “My favorite band is The Hold Steady!”
+
+
+
+3. In order to make the method we wrote in problem 2 print “My favorite band is (your favorite band input here)!” to the screen, what do you need to do?
+
+
+
+4. There is an error in the following method. It will not work the way we want it to! What should be changed to set things straight?
+
+5. Let’s try writing a method that takes in two inputs. Write a method that takes in the first name and last name of your favorite musician and then uses those inputs to state who your favorite musician is.
+
+             Example output: “My favorite musician is Duane Allman!”
+
+6. There is an error in the following program. It won’t run! What is wrong here?
+
+7. Can you explain what happens when the following method is called? What would be the output? Try using the “Rubber Ducky Approach” (talk through it out loud to yourself).
+
+8. Can you explain what happens when the following method is called? What would be the output?
+
+9. In the following method, what gets saved into y?
+
+10. In the following method, what gets saved into y?
+=end
