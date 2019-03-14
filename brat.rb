559ae2e2e58d2c03dd8fbcc1aa636b50a0071c74
Elliott C. Woodward <elliottcwoodward@gmail.com>
2019-03-13 22:24:09 -0400

    added loops.rb

--- brat.rb
+++ brat.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby -wKU
 
+# Did your brother or sister ever copycat everything you said
+# just to get under your skin? Man, was that annoying or what?!
+# Let's write a program that does the same thing.
+# This annoying program can only be stopped if the user says
+# "I'm  a dummy" because the program won't repeat something
+# so self-deprecating!
+
 until (a = gets.chomp.downcase) == "i'm a dummy"
   puts a
 end
---
+++ loops.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby -wKU
+
+#1
+10.times { puts "What's up?" }
+
+#2 this example will output 0 through 5 each on a line
+6.times do |count|
+  puts count
+end
+
+#3
+array = [1, 2, 3, 4, 5]
+array.each { |x| puts x }
+
+#4
+(1..4).each { |x| puts array[x] }
+
+#5
+atl_neightborhoods = %w[Virginia\ Highlands Grant\ Park Buckhead]
+atl_neightborhoods.each { |count| puts count }
+
+puts atl_neightborhoods.count
+
+puts
+
+#6 error after 6 bananas
+# 6.times do
+#   x = 'banana'
+#   puts x
+# end
+# puts x
+
+puts
+
+#6 corrected, puts "banana" 7 times
+x = 'banana'
+6.times { puts x }
+puts x
+
+puts
+
+#7 puts banana 6 times, then puts banana (NOT 'apple')!!
+x = 'apple'
+6.times { x = 'banana'; puts x }
+puts x
